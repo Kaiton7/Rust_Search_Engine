05a632c365d707b87b9c276392574c252fd9b099
uemurakaito <k-uemr@mats.ist.osaka-u.ac.jp>
2019-02-12 00:44:04 +0900

    change the data

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@
 extern crate scraper;
 extern crate mongodb;
 use scraper::Html;
-use std::io;
-use std::thread;
-use std::time::Duration;
-use std::sync::{Mutex, Arc};
+
 use mongodb::{Bson, bson, doc};
 use mongodb::{Client, ThreadedClient};
 use mongodb::db::ThreadedDatabase;
@@ -15,31 +12,49 @@ fn main() {
     let client = Client::connect("localhost", 27017)
         .expect("Failed to initialize standalone client.");
 
-    let coll = client.db("test").collection("movies");
+    //let coll = client.db("test").collection("movies");
+    let coll = client.db("index").collection("url");
 
-    let doc = doc! {
-        "title": "Jaws",
-        "array": [ 1, 2, 3 ],
+    let doc_RSE = doc! {
+        "keyword": "today",
+        "url": ["https:/local/","https:test"],
     };
+    //let doc = doc! {
+    //    "title": "Jaws",
+    //    "array": [ 1, 2, 3 ],
+    //};
 
+    coll.insert_one(doc_RSE.clone(), None)
+        .ok().expect("Failed insert");
     // Insert document into 'test.movies' collection
-    coll.insert_one(doc.clone(), None)
-        .ok().expect("Failed to insert document.");
+    //coll.insert_one(doc.clone(), None)
+    //    .ok().expect("Failed to insert document.");
 
+    let mut cursor_RSE = coll.find(Some(doc_RSE.clone()), None)
+        .ok().expect("Failed find");
     // Find the document and receive a cursor
-    let mut cursor = coll.find(Some(doc.clone()), None)
+    /* let mut cursor = coll.find(Some(doc.clone()), None)
         .ok().expect("Failed to execute find.");
+ */
+    let item_RSE = cursor_RSE.next();
+    //let item = cursor.next();
 
-    let item = cursor.next();
-
+    match item_RSE {
+        Some(Ok(doc_RSE)) => match doc_RSE.get("keyword") {
+            Some(&Bson::String(ref keyword)) => println!("{}", keyword),
+            _ => panic!("Expected title to be a string!"),
+        },
+        Some(Err(_)) => panic!("Failed to get next from server!"),
+        None => panic!("Server returned no results!"),
+    }
     // cursor.next() returns an Option<Result<Document>>
-    match item {
+    /* match item {
         Some(Ok(doc)) => match doc.get("title") {
             Some(&Bson::String(ref title)) => println!("{}", title),
             _ => panic!("Expected title to be a string!"),
         },
         Some(Err(_)) => panic!("Failed to get next from server!"),
         None => panic!("Server returned no results!"),
-    }
+    } */
 
 }
